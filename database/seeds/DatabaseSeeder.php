71722d06e0039ae5eb92c0078e1a821899e2da15
Mocha. N. Fadillah <mochamadfadilah88@gmail.com>
2018-11-11 20:35:24 +0700

    call seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        $this->call(PaymentMethodTableSeeder::class);
+        $this->call(DepositTableSeeder::class);
     }
 }
