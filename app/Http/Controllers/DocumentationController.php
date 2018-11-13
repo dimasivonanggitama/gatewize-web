96969c04a22643766aba29b569694777bae6893b
Rama Zeta <ramazeta1997@gmail.com>
2018-11-14 03:58:05 +0700

    Add DocumentationController

---
+++ app/Http/Controllers/DocumentationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use App\Deposit as Deposit;
+
+class DocumentationController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function index()
+    {
+        $deposit = Deposit::where(['user_id' => Auth::user()->id])->first();
+        return view('admin.pages.documentation.index', compact('deposit'));
+    }
+}
