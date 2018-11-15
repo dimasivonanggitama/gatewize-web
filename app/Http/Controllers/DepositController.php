da928731ab8ed23394b428428c7ef3929fd7fa94
Mocha. N. Fadillah <mochamadfadilah88@gmail.com>
2018-11-16 00:25:34 +0700

    create callback deposit

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -15,13 +15,13 @@ class DepositController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['getCallback']]);
 
         $this->middleware(function ($request, $next) {
             $this->data['deposit'] = Deposit::where(['user_id' => Auth::user()->id])->first();
 
             return $next($request);
-        });
+        }, ['except' => ['getCallback']]);
     }
     
     public function index()
@@ -45,7 +45,7 @@ class DepositController extends Controller
 
         $now = Carbon::now();
         $expired_date = $now->addHours(6);
-
+        $unique_code = rand(0, 999);
         $data = [
             'user_id' => Auth::user()->id,
             'payment_method_id' => $request->payment,
@@ -53,7 +53,8 @@ class DepositController extends Controller
             'balance' => 0,
             'sender_name' => $request->sender,
             'status' => 'WAITING',
-            'unique_code' => rand(0, 999),
+            'unique_code' => $unique_code,
+            'total' => $request->amount + $unique_code,
             'expired_date' => $expired_date
         ];
 
@@ -79,5 +80,43 @@ class DepositController extends Controller
         $this->data['newDeposit'] = $deposit;
         $pdf = PDF::loadView('admin.pages.deposit.invoice-print', $this->data);
         return $pdf->download('invoice.pdf');
+    }
+
+    public function getCallback(Request $request){
+        $result = $request->all();
+        print_r($result);
+        if( $result['action'] == "payment_report" )
+        {
+            foreach( $result['content']['data'] as $data )
+            {
+                # Waktu transaksi dalam format unix timestamp
+                $time = $data['unix_timestamp'];
+                $date = date('Y-m-d h:i:s', $time);
+                echo "<br><h1>$date</h1>";
+
+                # Tipe transaksi : credit / debit
+                $type = $data['type'];
+
+                # Jumlah (2 desimal) : 50000.00
+                $amount = $data['amount'];
+                $unique_code = \substr($amount, strlen($amount)-3);
+
+                # Berita transfer
+                $description = $data['description'];
+
+                # Saldo rekening (2 desimal) : 1500000.00
+                $balance = $data['balance'];
+                
+                if( $type == "credit" )
+                {
+                    $deposit = Deposit::where(['total' => $amount])->orderBy('id', 'desc')->firstOrFail();
+                    if($deposit != null){
+                        $deposit->status = 'ACCEPTED';
+                        $deposit->balance = $deposit->balance + $deposit->amount;
+                        $deposit->save();
+                    }
+                }
+            }
+        }
     }
 }
