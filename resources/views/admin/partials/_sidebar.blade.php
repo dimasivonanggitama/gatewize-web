4dfa0b55bdf5ae6ed267426c44f11e50a66ae1c1
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2019-03-20 19:08:31 +0700

    in progress edit sidebar

--- resources/views/admin/partials/_sidebar.blade.php
+++ resources/views/admin/partials/_sidebar.blade.php
@@ -26,6 +26,163 @@
       </a>
     </li>
     <li class="nav-item">
+      <a class="nav-link" data-toggle="collapse1" href="#service" aria-expanded="false" aria-controls="service">
+        <i class="menu-icon mdi mdi-currency-usd"></i>
+        <span class="menu-title">Service</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse1" id="service">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#digipos" aria-expanded="false" aria-controls="service">
+              <span class="menu-title">Digipos</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="digipos">
+              <ul class="nav flex-column sub-menu" style="padding: 0 0 0 1rem;">
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit-add') }}">Groups</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Accounts</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Reports</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Products</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Documentation</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#gojek" aria-expanded="false" aria-controls="gojek">
+              <span class="menu-title">GOJEK</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="gojek">
+              <ul class="nav flex-column sub-menu" style="padding: 0 0 0 1rem;">
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit-add') }}">Groups</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Accounts</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Reports</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Products</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Documentation</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ovo" aria-expanded="false" aria-controls="ovo">
+              <span class="menu-title">OVO</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ovo">
+              <ul class="nav flex-column sub-menu" style="padding: 0 0 0 1rem;">
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit-add') }}">Groups</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Accounts</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Reports</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Products</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Documentation</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#linkaja" aria-expanded="false" aria-controls="linkaja">
+              <span class="menu-title">Link Aja!</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="linkaja">
+              <ul class="nav flex-column sub-menu" style="padding: 0 0 0 1rem;">
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit-add') }}">Groups</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Accounts</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Reports</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Products</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('deposit') }}">Documentation</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="collapse" href="#billingbalance" aria-expanded="false" aria-controls="billingbalance">
+        <i class="menu-icon mdi mdi-currency-usd"></i>
+        <span class="menu-title">Billing & Balance</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="billingbalance">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('deposit-add') }}">Tambah Deposit</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('deposit') }}">Riwayat Deposit</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('deposit') }}">Riwayat Transaksi</a>
+          </li>
+        </ul>
+      </div>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="collapse" href="#support" aria-expanded="false" aria-controls="service">
+        <i class="menu-icon mdi mdi-currency-usd"></i>
+        <span class="menu-title">Support</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="support">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('deposit-add') }}">Open Ticket</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('deposit') }}">Term of Service</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('deposit') }}">Privacy Policy</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('deposit') }}">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </li>
+
+<!--     <li class="nav-item">
       <a class="nav-link" data-toggle="collapse" href="#deposit" aria-expanded="false" aria-controls="deposit">
         <i class="menu-icon mdi mdi-currency-usd"></i>
         <span class="menu-title">Deposit</span>
@@ -72,5 +229,22 @@
         <span class="menu-title">Billing</span>
       </a>
     </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="collapse" href="#product" aria-expanded="false" aria-controls="product">
+        <i class="menu-icon mdi mdi-currency-usd"></i>
+        <span class="menu-title">Products</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="product">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('product_pulsa') }}">Pulsa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('product_token') }}">Token</a>
+          </li>
+        </ul>
+      </div>
+    </li> -->
   </ul>
 </nav>
