d9f3041a212c9fa73686ecd321defc56a1fae827
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2019-03-23 23:45:45 +0700

    add show tickets comments

--- resources/views/admin/pages/ticket/show.blade.php
+++ resources/views/admin/pages/ticket/show.blade.php
@@ -12,10 +12,13 @@
 			<h4 class="card-title">{{$ticket->title}}</h4>
 			<div class="form-group row">
 				<label for="exampleInputPassword2" class="col-sm-3 col-form-label">Category</label>
-				<p>{{$ticket->category->name}}</p>
+				<div class="col-sm-9">
+					<input type="text" class="form-control" id="exampleInputPassword2" value="{{$ticket->category->name}}" disabled>
+				</div>
 			</div>
 			<div class="form-group row">
 				<label for="exampleInputPassword2" class="col-sm-3 col-form-label">Status</label>
+
 				@if($ticket->status == 'open')
 				<label class="badge badge-success">{{$ticket->status}}</label>
 				@else
@@ -24,18 +27,27 @@
 			</div>
 			<div class="form-group row">
 				<label for="exampleInputPassword2" class="col-sm-3 col-form-label">Created at</label>
-				<label>{{$ticket->created_at}}</label>
+				<div class="col-sm-9">
+					<input type="text" class="form-control" id="exampleInputPassword2" value="{{$ticket->created_at}}" disabled>
+				</div>
 			</div>
 			<hr>
-
+			<label>Comments</label>
+			@foreach ($comments as $comment)
+			<div class="form-group">
+					<label for="exampleTextarea1">{{ $comment->user->fullname }} - {{ $comment->created_at->diffForHumans() }}</label>
+					<textarea class="form-control" id="exampleTextarea1" rows="4" disabled>{{ $comment->comment }}</textarea>
+			</div>
+			<hr>
+			@endforeach
 			<form class="forms-sample" method="POST" action="/admin/comment">
 				{{csrf_field()}}
 				<input type="hidden" name="ticket_id" value="{{ $ticket->id }}">
 				<div class="form-group">
-					<label for="exampleTextarea1">Comment</label>
+					<label for="exampleTextarea1">Reply Ticket</label>
 					<textarea class="form-control" id="exampleTextarea1" rows="6" name="comment"></textarea>
 				</div>
-				<button type="submit" class="btn btn-primary" style="float: right;">Submit</button>
+				<button type="submit" class="btn btn-primary" style="float: right;">Reply</button>
 			</form>
 		</div>
 	</div>
