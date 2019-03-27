57f91f830876cc25d9894602f0a360c55c1f21c5
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2019-03-27 22:51:23 +0700

    Add open ticket dialog

--- resources/views/admin/pages/ticket/index.blade.php
+++ resources/views/admin/pages/ticket/index.blade.php
@@ -4,7 +4,8 @@
 <div class="card">
 	<div class="card-body">
 		<h4 class="card-title">Ticket List</h4>
-			<a href="{{route('ticket.create')}}" class="btn btn-success mr-2" style="color: white">New Ticket</a>
+		<button type="button"  class="btn btn-success mr-2" style="color: white" data-toggle="modal" data-target="#createModal">New Ticket</button>
+		<!-- <a href="{{route('ticket.create')}}" class="btn btn-success mr-2" style="color: white">New Ticket</a> -->
 		<div class="table-responsive">
 			<table class="table table-hover">
 				<thead>
@@ -32,6 +33,57 @@
 					@endforeach
 				</tbody>
 			</table>
+		</div>
+	</div>
+</div>
+<div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="createModal" aria-hidden="true">
+	<div class="modal-dialog" role="document" style="margin-top: 10px;">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title">Open Ticket</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				@if (session('status'))
+				<div class="alert alert-success">
+					{{ session('status') }}
+				</div>
+				@endif
+			</div>
+			<form class="forms-sample" action="{{route('ticket.create')}}" method="POST">
+				<div class="modal-body">
+					<!-- <h4 class="card-title">Open Ticket</h4> -->
+					{{csrf_field()}}
+					<div class="form-group">
+						<label for="exampleInputName1">Title</label>
+						<input type="text" class="form-control" id="exampleInputName1" name="title">
+					</div>
+					<div class="form-group">
+						<label for="exampleFormControlSelect1">Category</label>
+						<select class="form-control form-control-lg" id="exampleFormControlSelect1" name="category">
+							@foreach($categories as $category)
+							<option value="{{$category->id}}">{{$category->name}}</option>
+							@endforeach
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="exampleFormControlSelect1">Priority</label>
+						<select class="form-control form-control-lg" id="exampleFormControlSelect1" name="priority">
+							<option value="low">Low</option>
+							<option value="medium">Medium</option>
+							<option value="high">High</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="exampleTextarea1">Message</label>
+						<textarea class="form-control" id="exampleTextarea1" rows="6" name="message"></textarea>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary">Open Ticket</button>
+				</div>
+			</form>
 		</div>
 	</div>
 </div>
