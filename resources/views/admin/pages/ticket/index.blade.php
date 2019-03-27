4fa44e6ca4d881bc43109e9172dfb615d2c86bfb
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2019-03-27 22:21:16 +0700

    ticket index to sidebar

--- resources/views/admin/pages/ticket/index.blade.php
+++ resources/views/admin/pages/ticket/index.blade.php
@@ -4,6 +4,7 @@
 <div class="card">
 	<div class="card-body">
 		<h4 class="card-title">Ticket List</h4>
+			<a href="{{route('ticket.create')}}" class="btn btn-success mr-2" style="color: white">New Ticket</a>
 		<div class="table-responsive">
 			<table class="table table-hover">
 				<thead>
@@ -16,18 +17,18 @@
 				</thead>
 				<tbody>
 					@foreach($tickets as $ticket)
-						<tr class="clickable-row" data-href="{{route('ticket.show', $ticket->ticket_id)}}">
-							<td>{{$ticket->category->name}}</td>
-							<td>{{$ticket->title}}</td>
-							<td>
-								@if($ticket->status == 'open')
-								<label class="badge badge-success">{{$ticket->status}}</label>
-								@else
-								<label class="badge badge-danger">{{$ticket->status}}</label>
-								@endif
-							</td>
-							<td>{{$ticket->updated_at}}</td>
-						</tr>
+					<tr class="clickable-row" data-href="{{route('ticket.show', $ticket->ticket_id)}}">
+						<td>{{$ticket->category->name}}</td>
+						<td>{{$ticket->title}}</td>
+						<td>
+							@if($ticket->status == 'open')
+							<label class="badge badge-success">{{$ticket->status}}</label>
+							@else
+							<label class="badge badge-danger">{{$ticket->status}}</label>
+							@endif
+						</td>
+						<td>{{$ticket->updated_at}}</td>
+					</tr>
 					@endforeach
 				</tbody>
 			</table>
