2a7924d717ee00040c9e14b66e6a0466282da9cb
Rama Zeta <ramazeta1997@gmail.com>
2019-04-02 21:23:31 +0700

    Update Dashboard Blade

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -65,6 +65,30 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-12 grid-margin">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center mb-4">
+                      <h2 class="card-title mb-0">Product Analysis</h2>
+                      <div class="wrapper d-flex">
+                        <div class="d-flex align-items-center mr-3">
+                          <span class="dot-indicator bg-success"></span>
+                          <p class="mb-0 ml-2 text-muted">Product</p>
+                        </div>
+                        <div class="d-flex align-items-center">
+                          <span class="dot-indicator bg-primary"></span>
+                          <p class="mb-0 ml-2 text-muted">Resources</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="chart-container"><div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+                      <canvas id="dashboard-area-chart" height="572" width="2146" class="chartjs-render-monitor" style="display: block; height: 286px; width: 1073px;"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('custom_js')
