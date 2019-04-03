24288b164e8f844248e0e3de5cd179e645578d5f
Rama Zeta <ramazeta1997@gmail.com>
2019-04-03 18:14:17 +0700

    Update Login Page Views

--- resources/views/auth/app.blade.php
+++ resources/views/auth/app.blade.php
@@ -11,36 +11,36 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- plugins:css -->
-    <link rel="stylesheet" href="{!! asset('theme/StarAdmin/vendors/iconfonts/mdi/css/materialdesignicons.min.css') !!}">
-    <link rel="stylesheet" href="{!! asset('theme/StarAdmin/vendors/css/vendor.bundle.base.css') !!}">
-    <link rel="stylesheet" href="{!! asset('theme/StarAdmin/vendors/css/vendor.bundle.addons.css') !!}">
+    <link rel="stylesheet" href="{!! asset('template/backend/assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css') !!}">
+    <link rel="stylesheet" href="{!! asset('template/backend/assets/vendors/css/vendor.bundle.base.css') !!}">
+    <link rel="stylesheet" href="{!! asset('template/backend/assets/vendors/css/vendor.bundle.addons.css') !!}">
     <!-- endinject -->
     <!-- plugin css for this page -->
     <!-- End plugin css for this page -->
     <!-- inject:css -->
-    <link rel="stylesheet" href="{!! asset('theme/StarAdmin/css/style.css') !!}">
+    <link rel="stylesheet" href="{!! asset('template/backend/assets/css/style.css') !!}">
     <!-- endinject -->
-    <link rel="shortcut icon" href="{!! asset('theme/StarAdmin/images/favicon.png') !!}" />
+    <link rel="shortcut icon" href="{!! asset('template/backend/assets/images/favicon.png') !!}" />
 </head>
 
 <body>
     <div class="container-scroller">
         <div class="container-fluid page-body-wrapper full-page-wrapper auth-page">
-            
+
             @yield('content')
-            
+
         </div>
     </div>
 
     <!-- container-scroller -->
     <!-- plugins:js -->
-    <script src="{!! asset('theme/StarAdmin/vendors/js/vendor.bundle.base.js') !!}"></script>
-    <script src="{!! asset('theme/StarAdmin/vendors/js/vendor.bundle.addons.js') !!}"></script>
+    <script src="{!! asset('template/backend/assets/vendors/js/vendor.bundle.base.js') !!}"></script>
+    <script src="{!! asset('template/backend/assets/vendors/js/vendor.bundle.addons.js') !!}"></script>
     <!-- endinject -->
 
     <!-- inject:js -->
-    <script src="{!! asset('theme/StarAdmin/js/off-canvas.js') !!}"></script>
-    <script src="{!! asset('theme/StarAdmin/js/misc.js') !!}"></script>
+    <script src="{!! asset('template/backend/assets/js/off-canvas.js') !!}"></script>
+    <script src="{!! asset('template/backend/assets/js/misc.js') !!}"></script>
     <!-- endinject -->
 </body>
 
