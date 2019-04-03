2067b20a6b4cefeeb7034a668af413f92ba9914d
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2019-04-03 21:39:53 +0700

    fix mass assigment issues

--- app/Deposit.php
+++ app/Deposit.php
@@ -2,14 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
 
 class Deposit extends Model
 {
-    protected $fillable = [
-        'user_id', 'payment_method_id', 'amount', 'balance', 'sender_name', 'total','status', 'unique_code', 'expired_date'
-    ];
-
     public function users(){
         return $this->belongsTo('App\User', 'user_id');
     }
--- app/Product.php
+++ app/Product.php
@@ -2,12 +2,8 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-
 class Product extends Model
 {
-    protected $guarded = [];
-
     public function service()
     {
     	return $this->belongsTo(Service::class);
