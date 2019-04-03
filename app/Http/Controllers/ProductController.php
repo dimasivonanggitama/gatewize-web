01d5e8d471da625f5dc9fe5c345f8270a410e524
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2019-04-03 21:39:19 +0700

    fix store backend

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -35,6 +35,54 @@ class ProductController extends Controller
 		return view('admin.pages.product.create', compact('services'));
 	}
 
+	public function subscribe(Request $request)
+	{
+		$this->validate($request, [
+			'product_id' => 'required|exists:products,id'
+		]);
+		$product = Product::find($request->product_id);
+		$user = auth()->user();
+		if ($product != null) {
+			$balanceLeft = $user->balance - $product->price;
+			if ($balanceLeft > 0) {
+				$client;
+				switch (strtolower($product->service->name)) {
+					case 'gojek':
+					$client = new GojekClient();
+					break;
+					case 'digipos':
+					$client = new DigiposClient();
+					break;
+					case 'ovo':
+					$client = new OvoClient();
+					break;
+				}
+				$response = $client->subscribe($user->id, $user->license_key, $product->slot, $product->service->settings);
+				if ($response['status']) {
+					$user->balanceHistories()->create([
+						'type' => 'credit',
+						'description' => 'Pembelian produk '. $product->name,
+						'last_balance' => $user->balance,
+						'update_balance' => $balanceLeft
+					]);
+					$user->update([
+						'balance' => $balanceLeft
+					]);
+					flash('Product subscribe succeed.')->success();
+					return back();
+				}
+				flash('There\'s something wrong with server')->error();
+				return back();
+			}else{
+				flash('Your balance doesn\'t enough')->error();
+				return back();
+			}
+		}else{
+			flash('Product not found')->error();
+			return back();
+		}
+	}
+
 	public function store(Request $request)
 	{
 		$this->validate($request, [
@@ -116,13 +164,13 @@ class ProductController extends Controller
 	{
 		$client = new GojekClient();
 		$license = auth()->user()->license_key;
-        $tsProducts = $client->pulsaProducts($license, '082268888859');
-        $xlProducts = $client->pulsaProducts($license, '087880892109');
-        $axProducts = $client->pulsaProducts($license, '083808921095');
-        $thProducts = $client->pulsaProducts($license, '08973502424');
-        $idProducts = $client->pulsaProducts($license, '085746089296');
-        $smProducts = $client->pulsaProducts($license, '08812345678');
-        $pulsaProducts = array_merge($tsProducts, $xlProducts, $axProducts, $thProducts, $smProducts, $idProducts);
+		$tsProducts = $client->pulsaProducts($license, '082268888859');
+		$xlProducts = $client->pulsaProducts($license, '087880892109');
+		$axProducts = $client->pulsaProducts($license, '083808921095');
+		$thProducts = $client->pulsaProducts($license, '08973502424');
+		$idProducts = $client->pulsaProducts($license, '085746089296');
+		$smProducts = $client->pulsaProducts($license, '08812345678');
+		$pulsaProducts = array_merge($tsProducts, $xlProducts, $axProducts, $thProducts, $smProducts, $idProducts);
 		$tokenProducts = $client->tokenProducts($license);
 		return view('admin.pages.product.gojek', compact('pulsaProducts', 'tokenProducts'));
 	}
@@ -145,13 +193,13 @@ class ProductController extends Controller
 	{
 		$client = new OvoClient();
 		$license = auth()->user()->license_key;
-        $tsProducts = $client->pulsaProducts($license, 'TELKOMSEL');
-        $xlProducts = $client->pulsaProducts($license, 'XL');
-        $axProducts = $client->pulsaProducts($license, 'AXIS');
-        $thProducts = $client->pulsaProducts($license, 'TRI');
-        $idProducts = $client->pulsaProducts($license, 'INDOSAT');
-        $smProducts = $client->pulsaProducts($license, 'SMARTFREN');
-        $pulsaProducts = array_merge($tsProducts, $xlProducts, $axProducts, $thProducts, $smProducts, $idProducts);
+		$tsProducts = $client->pulsaProducts($license, 'TELKOMSEL');
+		$xlProducts = $client->pulsaProducts($license, 'XL');
+		$axProducts = $client->pulsaProducts($license, 'AXIS');
+		$thProducts = $client->pulsaProducts($license, 'TRI');
+		$idProducts = $client->pulsaProducts($license, 'INDOSAT');
+		$smProducts = $client->pulsaProducts($license, 'SMARTFREN');
+		$pulsaProducts = array_merge($tsProducts, $xlProducts, $axProducts, $thProducts, $smProducts, $idProducts);
 		$bankProducts = $client->bankProducts($license);
 
 		return view('admin.pages.product.ovo', compact('pulsaProducts', 'bankProducts'));
