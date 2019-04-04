e3a4aa0ec015a3ae1e4b25f5c76d7738b5c1f016
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2019-04-04 13:40:59 +0700

    Update layout verify email

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,24 +1,24 @@
 @extends('home.app')
 
+@section('pageTitle', 'Verify Email')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+	<div class="container" style="margin-top:30px">
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<div class="card" style="pointer-events: none;">
+					<div class="card-header">{{ __('Verify Your Email Address') }}</div>
+					<div class="card-body">
+						@if (session('resent'))
+							<div class="alert alert-success" role="alert">
+								{{ __('A fresh verification link has been sent to your email address.') }}
+							</div>
+						@endif
+						{{ __('Before proceeding, please check your email for a verification link.') }}
+						{{ __('If you did not receive the email') }}, <a style="pointer-events: initial;" href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
